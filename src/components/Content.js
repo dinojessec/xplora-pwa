be16129f99430c34b39f4c2cfd8f21874fe7531f
Jesse Dino <jesse.dino.z3@gmail.com>
2019-07-09 12:26:10 +0800

    added datepicker package

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Calendar from "./calendar/Calendar";
+// import Calendar from "./calendar/Calendar";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Content() {
+  const [count, setCount] = useState(0);
+  const [dates, setDate] = useState({
+    startDate: new Date()
+  });
+  console.log(dates);
+  const handleChange = input => {
+    let month = "" + (input.getMonth() + 1),
+      day = "" + input.getDate(),
+      year = input.getFullYear(),
+      hours = input.getHours(),
+      minutes = input.getMinutes(),
+      seconds = input.getSeconds();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    // return [year, month, day].join('-');
+
+    console.log(input);
+    console.log(month);
+    console.log(day);
+    console.log(year);
+    setDate({
+      startDate:
+        [year, month, day].join("-") + " " + [hours, minutes, seconds].join(":")
+    });
+  };
+
   return (
     <div className="container">
+      <button onClick={() => setCount(count + 1)}>click me</button>
       <div className="card m-3">
-        <Calendar />
+        {count % 2 === 0 ? (
+          <DatePicker
+            dateFormat={"MMMM d, yyyy"}
+            selected={new Date()}
+            onChange={handleChange}
+          />
+        ) : (
+          ""
+        )}
         <img
           src={require("../assets/DSC00591.jpg")}
           className="card-img-top"
