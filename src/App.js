2e095ee6100ca714dfe4d94e95ebdd32bc760f7e
Jesse Dino <jesse.dino.z3@gmail.com>
2019-07-10 17:13:27 +0800

    new outline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
+import Suggestions from "./components/Suggestions";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Jumbotron />
+      <Suggestions />
       <Content />
       <Footer />
     </div>
--- src/components/calendar/Calendar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Calendar extends React.Component {
-  render() {
-    return <h1>Calendar</h1>;
-  }
-}
