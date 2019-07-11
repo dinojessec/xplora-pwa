1d3c023a0fd55d53acaf445ac101233a8c8d869c
Jesse Dino <jesse.dino.z3@gmail.com>
2019-07-11 09:03:01 +0800

    fixed responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,46 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Icon
-        </a>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light p-4 mr-4 ml-4">
+      <a className="navbar-brand ml-lg-5" href="/">
+        Icon
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbar">
+        <ul className="navbar-nav ml-auto mr-5">
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Booking
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Help
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Sign up / Log in
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
