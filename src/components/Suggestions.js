9abd73fc904e48b3d5e01d85d06e1e7eee243465
Jesse Dino <jesse.dino.z3@gmail.com>
2019-07-11 10:31:39 +0800

    fixed alignment of suggestion box

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,5 +1,48 @@
 import React from "react";
 
 export default function Suggestions() {
-  return <div className="container">suggestions</div>;
+  return (
+    <div className="suggestions">
+      <div className="p-2">
+        <button
+          type="button"
+          className="suggestions-button btn btn-outline-dark"
+        >
+          <strong>Day Tours</strong>
+        </button>
+      </div>
+      <div className="p-2">
+        <button
+          type="button"
+          className="suggestions-button btn btn-outline-dark"
+        >
+          <strong>Shows</strong>
+        </button>
+      </div>
+      <div className="p-2">
+        <button
+          type="button"
+          className="suggestions-button btn btn-outline-dark"
+        >
+          <strong>Attractions</strong>
+        </button>
+      </div>
+      <div className="p-2">
+        <button
+          type="button"
+          className="suggestions-button btn btn-outline-dark"
+        >
+          <strong>Food</strong>
+        </button>
+      </div>
+      <div className="p-2">
+        <button
+          type="button"
+          className="suggestions-button btn btn-outline-dark"
+        >
+          <strong>More filters</strong>
+        </button>
+      </div>
+    </div>
+  );
 }
