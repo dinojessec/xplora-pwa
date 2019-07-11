76b8d58ae9d91f1ae1c67bda85443faaeceea471
Jesse Dino <jesse.dino.z3@gmail.com>
2019-07-11 10:13:49 +0800

    quick fix on Header and Jumbotron

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light p-4 mr-4 ml-4">
+    <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-light p-4 mr-4 ml-4">
       <a className="navbar-brand ml-lg-5" href="/">
         Icon
       </a>
@@ -15,7 +15,7 @@ export default function Header() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon" />
+        <span className="navbar-toggler-icon justify-content-end" />
       </button>
 
       <div className="collapse navbar-collapse" id="navbar">
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function Jumbotron() {
   return (
-    <div className="container">
-      <h1>*image here*</h1>
+    <div className="jumbotron">
+      <img
+        className="jumbo-img"
+        src={require("../assets/DSC00591.jpg")}
+        alt="..."
+      />
+      <div className="welcome-message">
+        <h1>Welcome!</h1>
+      </div>
+      <div className="intro-message">
+        <h1>Introduction</h1>
+      </div>
     </div>
   );
 }
